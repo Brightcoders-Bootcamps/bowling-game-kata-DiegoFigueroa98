8fae82015dfc2452b05484bd22f21cfb65cac405
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2020-07-03 12:24:59 -0500

    Add two test to check correct game score
    Implement a test to check the score with a game of all rolls
    with one pin down.
    
    Implement a test to check the score with a game with a spare.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -11,4 +11,16 @@ describe 'Bowling game logic' do
     @game.roll_many_same_score(20, 0)
     expect(@game.score).to eq(0)
   end
+
+  it('Can get score of all rolls one pin down') do
+    @game.roll_many_same_score(20, 1)
+    expect(@game.score).to eq(20)
+  end
+
+  it('Can get score of a game with a spare') do
+    @game.roll_many_same_score(2, 5)
+    @game.roll(1)
+    @game.roll_many_same_score(17, 0)
+    expect(@game.score).to eq(12)
+  end
 end
