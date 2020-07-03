248d25cf282e27ef4c4aa264c1031728e048d13a
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2020-07-03 12:11:09 -0500

    Add a test for a gutter game
    Implement a test to check the score with a game of all rolls
    with gutter balls.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -3,7 +3,12 @@
 require_relative '../app/bowling.rb'
 
 describe 'Bowling game logic' do
-  it('Can make a new game') do
+  before do
     @game = Bowling.new
+  end
+
+  it('Can get score of all rolls with gutter balls') do
+    @game.roll_many_same_score(20, 0)
+    expect(@game.score).to eq(0)
   end
 end
