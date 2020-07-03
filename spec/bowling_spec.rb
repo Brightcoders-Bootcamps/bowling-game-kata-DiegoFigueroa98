7c3019b13e560aae1c857442ad3fe7a933152238
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2020-07-03 13:04:10 -0500

    Add three test to check correct game score
    Implement a test to check the score with a game with a strike.
    
    Implement a test to check the score of a full spares game.
    
    Implement a test to check the score of a full strikes game.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -23,4 +23,22 @@ describe 'Bowling game logic' do
     @game.roll_many_same_score(17, 0)
     expect(@game.score).to eq(12)
   end
+
+  it('Can get score of a game with a strike') do
+    @game.roll_many_same_score(1, 10)
+    @game.roll_many_same_score(2, 1)
+    @game.roll_many_same_score(16, 0)
+    expect(@game.score).to eq(14)
+  end
+
+  it('Can get score of a full spares game') do
+    @game.roll_many_same_score(21, 5)
+    expect(@game.score).to eq(150)
+  end
+
+  it('Can get score of a full strikes game') do
+    @game.roll_many_same_score(12, 10)
+    expect(@game.score).to eq(300)
+  end
+
 end
