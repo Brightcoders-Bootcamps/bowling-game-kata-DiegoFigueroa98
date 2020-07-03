9172d0001916f6f73cab492a5db484e21e992430
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2020-07-02 20:49:14 -0500

    Add the method named 'roll many with same score'
    Add a method to help make testing easier.

--- app/bowling.rb
+++ app/bowling.rb
@@ -10,10 +10,13 @@ class Bowling
     @rolls.push(pins)
   end
 
-  def score
-    result = 0
-    current_roll = 0
+  def roll_many_same_score(rolls_number, pins)
+    rolls_number.times do
+      roll(pins)
+    end
+  end
 
+  def score(result = 0, current_roll = 0)
     10.times do
       result += @rolls[current_roll] + @rolls[current_roll + 1]
       result += @rolls[current_roll + 2] if spare?(current_roll)
